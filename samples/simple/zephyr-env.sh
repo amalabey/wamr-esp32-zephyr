70511d53ac2715aa67df90b71c98d4b37b7faa0f
Amal Abeygunawardana <amalabey@gmail.com>
2019-09-07 17:48:17 +1000

    include env variables script

---
+++ samples/simple/zephyr-env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+source ../../zephyr-env.sh
+export ZEPHYR_TOOLCHAIN_VARIANT="espressif"
+export ESPRESSIF_TOOLCHAIN_PATH="$HOME/esp/xtensa-esp32-elf/"
+export ZEPHYR_SDK_INSTALL_DIR="$HOME/zephyr-sdk-0.10.3"
+export ESP_IDF_PATH="$HOME/zephyr-sdk-0.10.3/xtensa-zephyr-elf"
+export ZEPHYR_SDK_INSTALL_DIR="$HOME/zephyr-sdk-0.10.3/""
+export PATH="$HOME/zephyr-sdk-0.10.3/xtensa-zephyr-elf/bin:$PATH"
