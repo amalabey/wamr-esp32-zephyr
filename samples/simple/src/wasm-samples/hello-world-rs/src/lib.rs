ee8ee28ed95662842a370fb51a5c8eada593471c
Amal Abeygunawardana <amalabey@gmail.com>
2019-09-08 23:13:50 +1000

    add support for thread_sleep exported fun in rust

--- samples/simple/src/wasm-samples/hello-world-rs/src/lib.rs
+++ samples/simple/src/wasm-samples/hello-world-rs/src/lib.rs
@@ -1,24 +1,19 @@
-extern crate wee_alloc;
-
-// Use `wee_alloc` as the global allocator.
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
-//use wasm_bindgen::prelude::*;
 extern {
     pub fn print_test(num: i32);
+    pub fn thread_sleep(millis: i32);
 }
 
 #[no_mangle]
 pub extern fn main() {
-    let mut x: i32 = 444;
+    let mut x: i32 = 898;
     unsafe{ print_test(x);}    
     //println!("initialising: {0}", "rust");
 
     loop {
         x += 1;
         //println!("hello from rust loop: {0}", x);
-        unsafe {print_test(x)}
+        unsafe {print_test(x);}
+        unsafe {thread_sleep(2000);}
     }
 }
 
