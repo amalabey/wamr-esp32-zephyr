0a6a2159bd08edbfc449558fb1594fef599a29ac
Amal Abeygunawardana <amalabey@gmail.com>
2019-09-08 10:43:02 +1000

    start serial monitor after flashing

--- samples/simple/flash.sh
+++ samples/simple/flash.sh
@@ -5,3 +5,5 @@ esptool.py --chip esp32 elf2image build/zephyr/zephyr.elf
 echo "Attempting to flash /dev/ttyUSB0"
 esptool.py --chip esp32 --port /dev/ttyUSB0 --baud 1500000 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x1000 bootloader/bootloader_dio_40m.bin 0x8000 bootloader/partitions.bin 0xe000 bootloader/boot_app0.bin 0x10000 build/zephyr/zephyr.bin
 echo "Done.."
+echo "Opening serial port"
+miniterm.py /dev/ttyUSB0 115200
