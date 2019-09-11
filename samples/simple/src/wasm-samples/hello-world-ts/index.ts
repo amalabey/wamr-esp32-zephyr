6acd54c77c20aff312a995c92b594996ea560da4
Amal Abeygunawardana <amalabey@gmail.com>
2019-09-11 23:00:26 +1000

    typescript sample works for gpio

--- samples/simple/src/wasm-samples/hello-world-ts/index.ts
+++ samples/simple/src/wasm-samples/hello-world-ts/index.ts
@@ -5,9 +5,16 @@ declare function print_test(num: i32): void;
 @external("env","thread_sleep")
 declare function thread_sleep(num: i32): void;
 
+@external("env","gpio_out")
+declare function gpio_out(pin: i32, state: i32): void;
+
+
 export function main(): void {
+  let index: i32 = 0;
   while(true) {
     print_test(56);
-    thread_sleep(2000);
+    gpio_out(10, index % 2);
+    thread_sleep(500);
+    index += 1;
   }
 }
